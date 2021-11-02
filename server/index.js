b2195bd67304ffca1cb48ea54b3dec6d5410ec71
Jaleel <jaleeldeangalo@gmail.com>
2021-11-01 23:58:44 -0500

    Added WebSockets

--- server/index.js
+++ server/index.js
@@ -1,14 +1,47 @@
 const express = require("express")
 const app = express()
+const { createServer } = require("http")
+const { Server } = require("socket.io")
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+// Imports Connection Function
 const Connect = require("./config/connect")
 
+
+
+// Parsing JSON Data
 app.use(express.json())
+
+// Mounting Routes
 app.use("/api/auth", require("./routes/api/auth"))
 app.use("/api/post", require("./routes/api/post"))
 app.use("/api/user", require("./routes/api/user"))
 app.use("/api/comments", require("./routes/api/comments"))
 
+// Connects to MongoDB Atlas
 Connect()
 
+let connections = [] 
+
+io.on("connection", function(socket)  {
+        connections.push(socket)
+        console.log(`${connections.length} sockets are connected`)
+
+
+        socket.on("disconnect", function(data) {
+            connections.splice(connections.indexOf(socket), 1)
+            console.log(`${connections.length} sockets are connected`)
+        })
+
+        socket.on("Server Port", function(data) {
+            console.log(data)
+            io.emit("IOS Client Port", {Message: "Hi IOS Client"})
+        })
+  })
+
 const Port = process.env.PORT || 5500
-app.listen(Port, () => console.log(`Server running on port ${Port}`))
+httpServer.listen(Port, () => console.log(`Server running on port ${Port}`))
+
+  
+
