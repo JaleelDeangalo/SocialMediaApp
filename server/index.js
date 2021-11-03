7b8ee2cd4f23ff6fee89b591a375c436d9914f6a
Jaleel <jaleeldeangalo@gmail.com>
2021-11-03 01:52:33 -0500

    Removed WebSockets

--- server/index.js
+++ server/index.js
@@ -22,25 +22,35 @@ app.use("/api/comments", require("./routes/api/comments"))
 // Connects to MongoDB Atlas
 Connect()
 
+const Port = process.env.PORT || 5500
+
+/*
 let connections = [] 
 
 io.on("connection", function(socket)  {
         connections.push(socket)
-        console.log(`${connections.length} sockets are connected`)
+        console.log(`Socket connections: ${connections.length}`)
 
 
         socket.on("disconnect", function(data) {
             connections.splice(connections.indexOf(socket), 1)
-            console.log(`${connections.length} sockets are connected`)
+            console.log(`Socket connections: ${connections.length}`)
         })
 
-        socket.on("Server Port", function(data) {
+        socket.on("Server Port", function(data, room) {
             console.log(data)
-            io.emit("IOS Client Port", {Message: "Hi IOS Client"})
+            console.log(room)
+            io.emit("IOS Client Port", { Message: "Hi IOS Client from Server" })
+        })
+
+
+        socket.on("message", function(data, room) {
+            console.log(data)
+            io.emit("recieved", { Message: "Message Recieved "})
         })
   })
+  */
 
-const Port = process.env.PORT || 5500
 httpServer.listen(Port, () => console.log(`Server running on port ${Port}`))
 
   
