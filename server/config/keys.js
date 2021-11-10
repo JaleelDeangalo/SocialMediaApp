90c22e00f04ffd0bbf6d8ff3870bcb3e2f5d2b85
Jaleel <jaleeldeangalo@gmail.com>
2021-11-09 21:22:33 -0600

    Updated MongoDB Password

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUri: "mongodb+srv://Jaleel:Jaleel123@social-media.pok1e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongoUri: "mongodb+srv://Jaleel:Tech1212@social-media.pok1e.mongodb.net/ArtSketch?retryWrites=true&w=majority",
     secret: "mytoken"
 }
